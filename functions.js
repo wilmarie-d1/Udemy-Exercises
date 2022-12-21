575f4c69b02a2888b7c9b9ee1ffef129144d375a
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-12-21 12:53:39 -0600

    function exercise

---
+++ functions.js
@@ -0,0 +1,10 @@
+// It's time to write our first function!
+//
+// Define a function called printHeart that prints out the string "<3"
+//
+// Execute your function once
+
+function printHeart() {
+    console.log("<3")
+}
+printHeart();
