33753943ee28cd4aa3b0c779aa0a09683ab551b4
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-12-21 13:01:09 -0600

    beets exercise

--- functions.js
+++ functions.js
@@ -8,3 +8,17 @@ function printHeart() {
     console.log("<3")
 }
 printHeart();
+
+// Rant Exercise
+// Define a function called rant which accepts a string argument called message.  The function should print out an uppercased version of message 3 times (with 3 separate calls to console.log).  For example, rant("I hate beets") should print out:
+//
+//     I HATE BEETS
+// I HATE BEETS
+// I HATE BEETS
+
+function rant(message){
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+}
+rant();
