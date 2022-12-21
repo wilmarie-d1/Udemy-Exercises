2ff2dba0156f7efd3786c9613b23e212edd10550
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-12-21 14:01:39 -0600

    return day function

--- functions.js
+++ functions.js
@@ -149,3 +149,25 @@ function sumArray(nums) {
 console.log(sumArray([2, 5, 7]));
 console.log(sumArray([2,7, 7]));
 console.log(sumArray([]));
+
+
+// Write a function called returnDay. this function takes in one parameter (a number from 1-7) and returns the day of the week (1 is Monday, 2 is Tuesday, etc.)  If the number is less than 1 or greater than 7, the function should return null. In some countries Sunday is treated as the first day of the week, but for this exercise we will use Monday as the first day.
+//
+//     Hint: store the days of the week in an array, or use an object with numerical keys.  When the function is called, plug the number into the array/object you've created to retrieve the corresponding day name and then return that value.
+//
+// returnDay(1) // "Monday"
+// returnDay(7) // "Sunday"
+// returnDay(4) // "Thursday"
+// returnDay(0) // null
+
+function returnDay(num) {
+    let daysOfWeek = ['days','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    for (let i = 0; i < daysOfWeek.length; i++) {
+        if (num > 7 || num < 1) {
+            return null;
+        } else {
+            return daysOfWeek[num];
+        }
+    }
+}
+console.log(returnDay(3));
