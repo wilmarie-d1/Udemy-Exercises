9b5a0395fa3e011911f0e1e485c2a3385199984c
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-12-21 13:16:38 -0600

    mutiple args exercise

--- functions.js
+++ functions.js
@@ -16,9 +16,29 @@ printHeart();
 // I HATE BEETS
 // I HATE BEETS
 
-function rant(message){
-    console.log(message.toUpperCase());
-    console.log(message.toUpperCase());
-    console.log(message.toUpperCase());
+// function rant(message){
+//     console.log(message.toUpperCase());
+//     console.log(message.toUpperCase());
+//     console.log(message.toUpperCase());
+// }
+// rant();
+
+
+// Multiple Args Exercise
+// In some dice games like Craps, a roll of two 1's is called "Snake Eyes".  It's generally not a good roll.  Please write a function called isSnakeEyes, which accepts two numbers as inputs, representing two dice.  If the two numbers are both 1's, please print "Snake Eyes!" otherwise print "Not Snake Eyes!"
+//
+// isSnakeEyes(1,1) //Snake Eyes!
+// isSnakeEyes(1,5) //Not Snake Eyes!
+// isSnakeEyes(4,5) //Not Snake Eyes!
+// Hint: Normally a function will return a value, but for the sake of having this work with the Udemy interpreter we will be using console.log() instead of return, to print the output.
+
+
+function isSnakeEyes(num1, num2) {
+    if(num1 == 1 && num2 == 1){
+        console.log("Snake Eyes!");
+    } else {
+        console.log("Not Snake Eyes!");
+    }
 }
-rant();
+console.log(isSnakeEyes(1,2));
+console.log(isSnakeEyes(1,1));
