7d8bd0345de001c518fe6463299bd698cfedd9ba
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-12-21 13:52:04 -0600

    sum exercise

--- functions.js
+++ functions.js
@@ -122,3 +122,30 @@ function capitalize(word) {
 }
 
 console.log(capitalize('foodie'));
+
+
+// Sum Array Exercise
+// Write a function called sumArray which accepts a single argument: an array of numbers.  It should return the sum of all the numbers in the array.
+//
+// sumArray([1,2,3]) // 6
+// sumArray([2,2,2,2]) // 8
+// sumArray([50,50,1]) // 101
+// Hints:
+//
+//     You'll need a variable to keep track of the total.  It should start out as zero.
+//
+// Loop over the array and for each element, add it to the total variable.
+//
+//     After you have added every number to total, return total.
+
+function sumArray(nums) {
+    let total = 0;
+    for (let i = 0; i < nums.length; i++) {
+        total += nums[i];
+    }
+    return total;
+}
+
+console.log(sumArray([2, 5, 7]));
+console.log(sumArray([2,7, 7]));
+console.log(sumArray([]));
