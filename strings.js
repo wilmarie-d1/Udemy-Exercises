9df9e4458520e57f1796849ecdea37b7606e73f4
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-12-22 15:09:07 -0600

    die exercise

--- strings.js
+++ strings.js
@@ -60,3 +60,26 @@ const word = "skateboard"; //Don't change this line!
 let less = word.slice(5);
 let facialHair = less.replace('o', 'e');
 console.log(facialHair);
+
+// String Template Literal Exercise
+// I've provided you with two variables, die1 and die2 which represent six-sided dice.  Each variable holds a randomly generated integer from 1 to 6.  Please create a new variable called roll, which will be a string that displays each die as well as their sum.  Follow this pattern:
+//
+// die1: 3
+//
+// die2: 5
+//
+// roll: "You rolled a 3 and a 5. They sum to 8"
+//
+// Here is another example with different numbers:
+//
+//     die1: 6
+//
+// die2: 4,
+//
+//     roll: You rolled a 6 and a 4. They sum to 10"
+//
+const die1 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+const die2 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+
+let roll = `You rolled a ${die1} and a ${die2} Their sum is ${die1 + die2}`;
+console.log(roll)
