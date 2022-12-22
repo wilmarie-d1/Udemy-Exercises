789252c4957abe5650a20cbc7b1e86d83dd561f1
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-12-22 13:47:37 -0600

    split and replace exercise

--- strings.js
+++ strings.js
@@ -37,7 +37,26 @@ let quote = 'You had me at "hello" ';
 //My answer: "19981"
 
 
+// String Methods Practice
+// Let's get some practice working with string methods. I've provided you with a variable in index.js called message.  Without altering the original value stored in message, please define a variable called whisper that is a lowercased version of message, with all the extra space at the beginning and end removed.  You will need to use two string methods that we just covered.  Don't forget you can chain them together on a single line!
+const message = "    TASTE THE RAINBOW!  "; //DON'T CHANGE THIS LINE PLEASE!
+
+let whisper = message.toLowerCase().trim();
+console.log(whisper);
 
 
+// More String Methods Practice
+// I've provided you with a word variable, set to "skateboard".  Your goal is to use string methods on word, so that you end up with the string "beard".
+//
+// Use a string method to extract the "board" part of "skateboard"  (using the slice method)
+//
+// Replace the "o" in board with an "e" (using the replace method)
+//
+// Save the result in a variable called facialHair
+//
+// This is another exercise that is very easy to cheat! I can't verify that you actually call the correct string methods.  You could just copy and paste the answer into the variable. BUT IF YOU DO THAT YOU WON'T BE ABLE TO LIVE WITH THE GUILT.  IT WILL TEAR YOU APART FOR AS LONG AS YOU LIVE! So, please follow the spirit of the exercise :)
 
-
+const word = "skateboard"; //Don't change this line!
+let less = word.slice(5);
+let facialHair = less.replace('o', 'e');
+console.log(facialHair);
