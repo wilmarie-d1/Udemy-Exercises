2897231c68cff2b4c7a356a22e823c824f14d9d2
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-12-22 13:23:30 -0600

    quiz and my answers, all correct

---
+++ strings.js
@@ -0,0 +1,43 @@
+// Our First String Variables Practice
+// Please define the following string variables (you can use either let or const):
+//
+// bestColor should be set to the string purple (the best color on planet earth, as we all know);
+//
+// quote should be set to the string You had me at "hello" - be careful about quotation marks, we want double quotes inside the string.
+
+let bestColor = 'purple';
+let quote = 'You had me at "hello" ';
+
+//QUIZ TIME
+// Question 1:
+// Given the following string:
+//
+//     const animal = "hippopotamus"
+// Which character am I accessing?
+//
+//     animal[7]
+
+// My answer: "t"
+
+// Question 2:
+// What is stored in the combo variable?
+//
+// const city = 'Kyoto';
+// const country = 'Japan';
+// const combo = city + country;
+
+// My answer : "KyotoJapan"
+
+// Question 3:
+// What does the following return?
+//
+//     let year = "1998";
+// year + 1;
+
+//My answer: "19981"
+
+
+
+
+
+
