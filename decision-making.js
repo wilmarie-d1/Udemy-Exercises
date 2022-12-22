2e92bfd08cb7f5872f43491b2d9a942a483f0c61
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-12-22 15:29:17 -0600

    exercise on > < >= <= == === !== !=

---
+++ decision-making.js
@@ -0,0 +1,19 @@
+//QUIZ TIME
+// Question 1:
+// What does the following code evaluate to?
+//
+//     3 <= 2
+
+//My answer is: False
+
+// What does the following evaluate to?
+//
+//     9 == '9'
+
+//My answer is: true
+
+//What does the following code evaluate to?
+//
+// -5 >= -6
+
+//My answer is: true
--- strings.js
+++ strings.js
@@ -82,4 +82,4 @@ const die1 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
 const die2 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
 
 let roll = `You rolled a ${die1} and a ${die2} Their sum is ${die1 + die2}`;
-console.log(roll)
+console.log(roll);
