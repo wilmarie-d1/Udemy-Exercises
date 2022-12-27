d5e4d9adbdd3cebd40b3cd878cab7a099d422e18
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-12-27 14:02:06 -0600

    lotto exercise

---
+++ Arrays.js
@@ -0,0 +1,4 @@
+// Lotto Numbers Exercise
+// Let's get some practice creating arrays. Please define a variable called lottoNumbers and set it equal to an array containing any 6 numbers.  It's simple, but we've got to start somewhere!
+//
+let lottoNumbers = [10, 20, 30, 40, 50 ,60];
