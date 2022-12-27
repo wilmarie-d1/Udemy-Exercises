eb8635ec1c22e7fd588cbc38ddf4327ce60679f8
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-12-27 14:10:08 -0600

    airplane  exercise

--- Arrays.js
+++ Arrays.js
@@ -34,3 +34,18 @@ const planets = ['The Moon','Venus', 'Earth', 'Mars', 'Jupiter']; //DO NOT TOUCH
 planets.shift();
 planets.push('Saturn');
 planets.unshift("Mercury");
+
+//Nested Arrays Exercise
+// I've provided you with an array called airplaneSeats, which represents the rows of seats on a flight.  Our friend Hugo is looking to book a seat on our flight. Fortunately, there is one middle seat available (it's set to null currently).  Please update the array element that is currently null, to instead be "Hugo". (Please don't directly change the code on line 6 where I'm initially setting it to null.  This is an exercise in accessing data in nested arrays!)
+// DO NOT TOUCH!!! (please)
+const airplaneSeats = [
+    ['Ruth', 'Anthony', 'Stevie'],
+    ['Amelia', 'Pedro', 'Maya'],
+    ['Xavier', 'Ananya', 'Luis'],
+    ['Luke', null, 'Deniz'],
+    ['Rin', 'Sakura', 'Francisco']
+];
+
+// YOUR CODE GOES BELOW THIS LINE:
+
+airplaneSeats[3][1] = 'Hugo'
