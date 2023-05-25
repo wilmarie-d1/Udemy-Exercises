745cd1cdf2fbd4e26ee2c04fbf1b920a5799ea33
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-05-24 22:35:36 -0500

    my first number exercises

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,14 @@ public class HelloWorld {
         System.out.println("Hello Wilmarie");
         int myFirstNumer = 5;
         System.out.println(myFirstNumer);
+        myFirstNumer = 10;
+        System.out.println(myFirstNumer);
+        myFirstNumer = 1000;
+        System.out.println(myFirstNumer);
+        myFirstNumer = 10 + 5;
+        System.out.println(myFirstNumer);
+        myFirstNumer = (10 + 5) + (2 *10);
+        System.out.println(myFirstNumer);
+
     }
 }
