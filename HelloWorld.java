6778c0f25066025f56cf1a97238a01669bba9d7b
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-05-24 22:24:26 -0500

    my first value exercise

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Wilmarie");
+        int myFirstNumer = 5;
+        System.out.println(myFirstNumer);
     }
 }
