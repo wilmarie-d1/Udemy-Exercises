176067be2b0f70cc34e6c784e7aa6553710bc8fb
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-05-25 23:36:54 -0500

    exercise 2

--- Lesson2.java
+++ Lesson2.java
@@ -1,5 +1,12 @@
 public class Lesson2 {
     public static void main(String[] args) {
         int myValue = 1000;
+        int myMinIntValue = Integer.MIN_VALUE;
+        System.out.println(myMinIntValue);
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println(myMaxIntValue);
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Minimum Value = " + Integer.MIN_VALUE);
+        System.out.println("Integer Value Range (" + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE + ")");
     }
 }
